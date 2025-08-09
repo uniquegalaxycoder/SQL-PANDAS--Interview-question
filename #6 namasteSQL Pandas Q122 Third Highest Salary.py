b88924a722fdff08ae3753f5dbd72a3762ae0a15
Omkar Ghodake <117935715+uniquegalaxycoder@users.noreply.github.com>
2025-08-09 20:20:28 +0530

    Update #6 namasteSQL Pandas Q122 Third Highest Salary.py

--- #6 namasteSQL Pandas Q122 Third Highest Salary.py
+++ #6 namasteSQL Pandas Q122 Third Highest Salary.py
@@ -1,8 +1,29 @@
+'''
+Q.122. You are working with an employee database where each employee has a department ID and a salary. Your task is to find the third highest salary in each department. 
+If there is no third highest salary in a department, then the query should return salary as null for that department. Sort the output by department id.
 
+Assume that none of the employees have same salary in a particular department.
 
+Table: employees 
++---------------+----------+
+| COLUMN_NAME   | DATA_TYPE|
++---------------+----------+
+| employee_id   | int      |
+| department_id | int      |
+| salary        | int      |
++---------------+----------+
 
-
-
+Required Output :
++---------------+----------------------+
+| department_id | third_highest_salary |
++---------------+----------------------+
+|             1 |                 3000 |
+|             2 |                 NULL |
+|             3 |                 3500 |
+|             4 |                 NULL |
+|             5 |                 4200 |
++---------------+----------------------+  
+'''
 
 
 
